bd8db798c9e76666a301de656745ba535ceaa14c
Wilson Wang <Wilson.Wang@wilsonwang.org>
2019-03-11 22:24:31 +0000

    Add files via upload

--- project_1.m
+++ project_1.m
@@ -1,26 +1,23 @@
 clear
 clc
 
-% test cases
 test_case1_in = 0:5:255;
 test_case2_in = 0:1/8:255;
 test_case3_in = 0:1/1024:255;
-
-% get the double precision results for the test cases
 test_case1_out = resGen(test_case1_in);
 test_case2_out = resGen(test_case2_in);
 test_case3_out = resGen(test_case3_in);
 
 % generate the look up table
-% the passed-in number is the length of the look up table
 lut = arctan_lut_gen(16);
 
-lut_bin = lut_to_bin(lut);
+% generate the complete look up table
+lut_complete = arctan_lut_gen(31);
+lut_bin = lut_to_bin(lut_complete);
 
 % testing the CORDIC function defined in cordic.m
 input_angle = -0.5;
 res_x = cordic(lut, input_angle);
-
 fprintf("input = %f\n", input_angle);
 fprintf("cos = %f\n", res_x);
 
@@ -50,7 +47,6 @@ for i = 1:loop_count
 end
 
 confidence_level = confidence_level_count/loop_count;
-
 MSE_mean = mean(MSE_out);
  
 fprintf("MSE = %f\n", MSE_mean);
